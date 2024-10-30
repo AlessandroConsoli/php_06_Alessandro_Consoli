9a42cf672688bcb9e0ce2df46e35bd036c2423e6
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-10-30 18:22:42 +0100

    aggiunta versione con switch

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
 <?php
 
+// Versione con if elseif
+
+// for ($i=1; $i <= 100; $i++) { 
+//     if ((($i % 3) == 0) && (($i % 5) == 0) ) {
+//         echo('HACKADEMY') . "\n";
+//     }elseif (($i % 5) == 0) {
+//         echo('JAVASCRIPT') . "\n";
+//     }elseif (($i % 3) == 0) {
+//         echo('PHP') . "\n";
+//     }else {
+//         echo($i) . "\n";
+//     }
+// }
+
+
+
+
+// Versione con switch
+
 for ($i=1; $i <= 100; $i++) { 
-    if ((($i % 3) == 0) && (($i % 5) == 0) ) {
-        echo('HACKADEMY') . "\n";
-    }elseif (($i % 5) == 0) {
-        echo('JAVASCRIPT') . "\n";
-    }elseif (($i % 3) == 0) {
-        echo('PHP') . "\n";
-    }else {
-        echo($i) . "\n";
-    }
-}
+    switch ($i) {
+        case (($i % 3) == 0) && (($i % 5) == 0):
+            echo('HACKADEMY') . "\n";
+            break;
+
+        case ($i % 5) == 0:
+            echo('JAVASCRIPT') . "\n";
+            break;
+
+        case ($i % 3) == 0:
+            echo('PHP') . "\n";
+            break;
+        
+        default:
+            echo($i) . "\n";
+            break;
+    };
+};
